59c5542cb0ef99d0ecea99476806c689f60791a6
aaaa lee <aaaalee1@lalalalu.local>
2018-07-14 00:43:56 +1000

    placeholder

---
+++ windows-shopper/windows-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyTxtField.swift
+//  windows-shopper
+//
+//  Created by aaaa lee on 2018/7/14.
+//  Copyright © 2018年 eha. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+        
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
+    
+
+}
